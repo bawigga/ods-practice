ad0ced354103f3da34c58aa050e4dfb8af346281
Brian Wigginton <brianwigginton@gmail.com>
2015-03-10 00:06:04 -0500

    logrus package is cool!

--- heap/binary_heap.go
+++ heap/binary_heap.go
@@ -1,8 +1,14 @@
 package heap
 
 import (
-//"fmt"
+	//"fmt"
+	log "github.com/Sirupsen/logrus"
 )
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+	//log.SetLevel(log.InfoLevel)
+}
 
 type BinaryHeap struct {
 	nodes []int
@@ -26,9 +32,10 @@ func (b *BinaryHeap) parent(i int) int {
 }
 
 // balance ensures that the given index is properly places in the head
-func (b *BinaryHeap) balance(i int) int {
+func (b *BinaryHeap) bubble(i int) int {
 	p := b.parent(i)
 	for i > 0 && b.nodes[i] < b.nodes[p] {
+		log.WithFields(log.Fields{"index": i, "value": b.nodes[i], "nodes": b.nodes}).Debug("Bubbling")
 		v := b.nodes[i]
 		b.nodes[i] = b.nodes[p]
 		b.nodes[p] = v
@@ -40,26 +47,35 @@ func (b *BinaryHeap) balance(i int) int {
 
 // Add inserts the given value into the heap.
 func (b *BinaryHeap) Add(v int) int {
+	log.WithFields(log.Fields{"value": v}).Info("Adding value")
 	b.nodes = append(b.nodes, v)
-	return b.balance(len(b.nodes) - 1)
+	return b.bubble(len(b.nodes) - 1)
 }
 
 // Remove returns the highest priority item from the head,
 func (b *BinaryHeap) Remove() int {
+
 	if len(b.nodes) <= 0 {
+		log.Warn("Remove: Head is empty")
 		return -1
 	}
+
 	smallest := b.nodes[0]
+
+	log.WithFields(log.Fields{"value": smallest}).Info("Removing Value")
+
 	j := len(b.nodes) - 1
 	b.nodes[0] = b.nodes[j]
 	b.nodes = b.nodes[:j]
-	b.trickle(0)
+	if j > 0 {
+		b.trickle(0)
+	}
 	return smallest
 }
 
 // trickle rebalances the heap after a node removal
 func (b *BinaryHeap) trickle(i int) {
-	//fmt.Println("trickling index", i, b.nodes[i], b.nodes)
+	log.WithFields(log.Fields{"index": i, "value": b.nodes[i], "nodes": b.nodes}).Debug("Trickling")
 	lc, lcExists := b.left(i)
 	rc, rcExists := b.right(i)
 	j := -1
--- heap/binary_heap_test.go
+++ heap/binary_heap_test.go
@@ -13,7 +13,11 @@ func TestAdd(t *testing.T) {
 	heap := BinaryHeap{
 		nodes: []int{4, 9, 8, 17, 26, 50, 16, 19, 69, 32, 93, 55},
 	}
-	index := heap.Add(6)
+	index := heap.Add(24)
+	index = heap.Add(96)
+	index = heap.Add(62)
+	index = heap.Add(2)
+	index = heap.Add(8)
 	if index != 2 {
 		t.Errorf("Expected index %d, got %d", 2, index)
 	}
