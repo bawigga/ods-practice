6ec648129deeabed2e0209cb47294d469bb559cc
Brian Wigginton <brianwigginton@gmail.com>
2018-03-05 21:06:17 -0600

    mergesort implementation, quicksort cleanup

--- merge_sort/main.go
+++ merge_sort/main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
 
 func main() {
+	size := 20
+	data := randArray(size)
+	// mergesort(data)
+	fmt.Printf("%v unsorted\n", data)
+	fmt.Printf("%v   sorted\n", mergesort(data))
+}
+
+func randArray(size int) []int {
 	var a []int
-	size := 10
 	for i := 0; i < size; i++ {
 		a = append(a, rand.Intn(size))
 	}
-	mergesort(a)
+	return a
 }
 
 func mergesort(a []int) []int {
-	if len(a) < 2 {
+	// fmt.Printf("presort: %v\n", a)
+	if len(a) <= 1 {
 		return a
 	}
-
-	mid := len(a)
-	left := mergesort(a[:mid])
-	right := mergesort(a[mid+1:])
-	size := len(left) + len(right)
-	aux := make([]int, size)
-	i, j, k := 0, 0, 0
-	for k < size {
-		if i < len(left)-1 && left[i] > right[j] {
-			aux[k++] = left[i++]
+	m := len(a) / 2
+	left := mergesort(a[:m])
+	right := mergesort(a[m:])
+	sorted := []int{}
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] <= right[0] {
+			sorted = append(sorted, left[0])
+			left = left[1:]
+		} else {
+			sorted = append(sorted, right[0])
+			right = right[1:]
 		}
 	}
+	if len(left) > 0 {
+		sorted = append(sorted, left...)
+	} else {
+		sorted = append(sorted, right...)
+	}
+	// fmt.Printf("merged: %v\n", sorted)
+	return sorted
 }
--- qsort/main.go
+++ qsort/main.go
@@ -8,17 +8,18 @@ import (
 )
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	var a []int
-	size := 30
+	size := 10
 	for i := 0; i < size; i++ {
-		a = append(a, rand.Intn(3))
+		a = append(a, rand.Intn(49))
 	}
 	qsort(a, 0, len(a)-1)
 }
 
 func qsort(a []int, lo int, hi int) {
-	logrus.Info("--------------------------------------------------------------------------------")
-	logrus.Infof("%v qsort lo=%d, hi=%d", a, lo, hi)
+	logrus.Info("-----------------------------------------------------------------------------------------------")
+	// logrus.Infof("lo=%d, hi=%d\n", a, lo, hi)
 	if lo < hi {
 		// p := hoarePartition(a, lo, hi)
 		p := cormenPartition(a, lo, hi)
@@ -29,16 +30,20 @@ func qsort(a []int, lo int, hi int) {
 
 func cormenPartition(a []int, lo int, hi int) int {
 	pivot := a[hi]
-	logrus.Infof("pivot=%d", pivot)
+	logrus.Infof("pivot=%d lo=%d hi=%d", pivot, lo, hi)
 	i := lo - 1
 	for j := lo; j <= hi-1; j++ {
+		// logrus.Debugf("lo=%d hi=%d i=%d j=%d", lo, hi, i, j)
 		if a[j] < pivot {
+			// logrus.Debugf("swapping")
 			i++
 			a[i], a[j] = a[j], a[i]
 		}
+		logrus.Debugf("%v lo=%d hi=%d i=%d j=%d pivot=%d", a, lo, hi, i, j, pivot)
 	}
 	if a[hi] < a[i+1] {
 		a[i+1], a[hi] = a[hi], a[i+1]
+		logrus.Debugf("%v", a)
 	}
 	return i + 1
 }
